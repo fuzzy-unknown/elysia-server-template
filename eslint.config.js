ef43a1f32b11dcdac82408e3c2d812b481392de3
@油腻_w4v0 <16615812538@163.com>
2026-05-06 16:55:49 +0800

    change

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
+  vue: true,
   ignores: ['.vscode', 'node_modules', 'README.md', 'drizzle'],
   rules: {
     'no-console': 'off',
